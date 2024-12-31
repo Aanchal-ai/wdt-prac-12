ed5351e57bf334ffd760d896f3d7d0dfd63af45b
Aanchal-ai <alwayscompetitor@gmail.com>
2024-12-31 14:21:28 +0530

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,4 +0,0 @@
-a = 1000
-b = 500
-sum = a - b
-print(sum)                                                                                                                                                                                       
